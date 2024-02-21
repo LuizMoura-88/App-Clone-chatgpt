92dd5aa2b2c2ef186baf1cd8ddfd326ace27ee71
Luiz Moura <lgm_moura@hotmail.com>
2024-02-21 00:08:10 -0300

    bug

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_URL ??
-    "https://app-clone-chatgpt-nnc4y78yz-luiz-mouras-projects.vercel.app/",
+  baseURL: process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000",
 });
 
 export default axiosInstance;
