e09696d1f131f1e0fe161481bdd550a27ed33e61
Luiz Moura <lgm_moura@hotmail.com>
2024-02-26 18:14:21 -0300

    resolvendo bug-100

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance } from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
 });
 
 export default axiosInstance;
